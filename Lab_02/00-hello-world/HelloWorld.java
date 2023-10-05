dc89fa78f01fbae93c5f6d486a1a0be03e23147b
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-10-05 22:25:50 +0200

    Add exercise 00-hello-world and his README

---
+++ Lab_02/00-hello-world/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Esempio di prima applicazione
+ */
+class HelloWorld {
+    /*
+     * Main dell'applicazione
+     */
+    public static void main(String[] argv) {
+        /*
+         * Stampa "Hello World!" in standard output
+         */
+        System.out.println("Hello, world!");
+
+        System.out.println("Nuovo Messaggio" + (50 + 50 * 50));
+    }
+}
